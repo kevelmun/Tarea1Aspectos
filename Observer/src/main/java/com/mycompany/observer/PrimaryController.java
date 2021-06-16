afe26f6768e0ab9aa8e22c7ec191c3e19fb11fd8
lnsalaza <lnsalaza@espol.edu.ec>
2021-06-16 00:10:28 -0500

    V3

--- Observer/src/main/java/com/mycompany/observer/PrimaryController.java
+++ Observer/src/main/java/com/mycompany/observer/PrimaryController.java
@@ -1,8 +1,13 @@
 package com.mycompany.observer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -25,7 +30,9 @@ public class PrimaryController {
     private ImageView imageViewID;
     
     private Color color;
-    
+    @FXML
+    private ListView<String> listViewID;
+    ObservableList list = FXCollections.observableArrayList();
    
     @FXML
     private void changeToYellow() throws IOException {
@@ -48,9 +55,28 @@ public class PrimaryController {
         color = new Color(s);
         Image img= new Image(color.getPath(),640,480,false,false);
         this.imageViewID.setImage(img);
-        
+        leerReporte();
     }
     
+    private void leerReporte(){
+        try {
+            FileReader reader = new FileReader("log.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) { 
+                list.removeAll(list);
+                list.add(line);
+                listViewID.getItems().addAll(list);
+                
+            }
+            reader.close();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    
+    }
     
     
     
